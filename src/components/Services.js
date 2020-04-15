0214af2ed75d82a0d529401dad2a2e331a0e629d
boredasfawk <olonnyetaylor@gmail.com>
2020-04-15 15:19:56 -0700

    added some zombie ipsum

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,14 +6,14 @@ const Services = (props) => (
   <React.Fragment>
     <section id="features" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
+        <h2 className="text-3xl lg:text-5xl font-semibold">Services</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
               <p className="font-semibold text-xl">Service One</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                De braaaiiiins apocalypsi gorger omero prefrontal cortex undead
+                survivor fornix dictum mauris.
               </p>
             </Card>
           </div>
@@ -21,8 +21,8 @@ const Services = (props) => (
             <Card className="mb-8">
               <p className="font-semibold text-xl">Service Two</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                cerveau eorum defunctis cerebro go lum cerebro. Nescio brains
+                an Undead cervello zombies.
               </p>
             </Card>
           </div>
@@ -30,8 +30,8 @@ const Services = (props) => (
             <Card className="mb-8">
               <p className="font-semibold text-xl">Service Three</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                Sicut thalamus malus putrid brains voodoo horror.
+                Nigh basal ganglia tofth eliv ingdead.
               </p>
             </Card>
           </div>
