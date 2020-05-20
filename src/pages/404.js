2353f8d055255bdf14aeec09f581ce4764e4e4ee
boredasfawk <olonnyetaylor@gmail.com>
2020-05-19 18:07:28 -0700

    :)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,12 @@
 import React from "react"
 import Seo from "../components/Seo"
-import SitePagesLayout from "../components/SitePagesLayout"
 
-const NotFoundPage = ({ location }) => (
-  <SitePagesLayout location={{ location }}>
+const NotFoundPage = () => (
+  <React.Fragment>
     <Seo title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </SitePagesLayout>
+  </React.Fragment>
 )
 
 export default NotFoundPage
